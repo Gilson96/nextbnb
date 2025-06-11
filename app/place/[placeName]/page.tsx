a25996c669c9d7a01ac859e05f2414dfffe4e430
Gilson96 <grafael99@gmail.com>
2025-06-11 11:11:14 +0100

    fix prisma migrations, components layout and added assets

--- app/place/[placeName]/page.tsx
+++ app/place/[placeName]/page.tsx
@@ -2,35 +2,19 @@ import Header from "@/components/home/header";
 import PlaceHeader from "@/components/place/header";
 import RoomList from "@/components/place/roomList";
 import {
-  getBoltonRooms,
-  getOldHamRooms,
-  getSalfordRooms,
-  getStockportRooms,
   getHosts,
 } from "@/lib/actions/place.actions";
 import { ArrowLeft } from "lucide-react";
 
 const Place = async (props: { params: Promise<{ placeName: string }> }) => {
   const { placeName } = await props.params;
-  const oldHamRooms = await getOldHamRooms();
-  const salfordRooms = await getSalfordRooms();
-  const boltonRooms = await getBoltonRooms();
-  const stockportRooms = await getStockportRooms();
-  const hosts = await getHosts();
-
-  const showRoomsByPlace = () => {
-    if (placeName === "Oldham") return oldHamRooms;
-    if (placeName === "Salford") return salfordRooms;
-    if (placeName === "Bolton") return boltonRooms;
-    if (placeName === "Stockport") return stockportRooms;
-  };
 
   return (
     <main className="flex flex-col justify-between">
       <PlaceHeader />
       <div className="h-[10rem] w-full bg-neutral-500"></div>
       <div className="w-full flex-col rounded-t-lg border-t bg-white p-[5%]">
-        {showRoomsByPlace()?.map((room, index) => (
+        {/* {showRoomsByPlace()?.map((room, index) => (
           <RoomList
             key={index}
             id={room.id}
@@ -43,7 +27,7 @@ const Place = async (props: { params: Promise<{ placeName: string }> }) => {
               hosts.find((host) => host.id === room.hostId)?.hostingYears!
             }
           />
-        ))}
+        ))} */}
       </div>
     </main>
   );
